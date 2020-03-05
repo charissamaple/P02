fce1d8f4b32359213ccf442755f2d2d2f71b8544
Maple <charissamaple@gmail.com>
2020-03-04 17:57:14 -0800

    Trying to add javascript and fix hamburger menu
    turned off javascript code

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
 function showMenu(){
 
     var x = document.getElementById("navigation");
-        if (x.style.display === "none") {
+    var y = screen.width;
+    var mobile = 420;
+
+    if (x.style.display === "none" && y < mobile) {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
+
+    // if (y > mobile){
+    //     x.style.display = "block";
+    // }
 }
+
+
+
+
+// var hamburger = document.querySelector(".burger");
+// // On click
+// hamburger.addEventListener("click", function() {
+// // Toggle class "is-active"
+// hamburger.classList.toggle("is-active");
+//     document.getElementById("sideNav").classList.toggle("active");
+//     document.getElementById("topNav").classList.toggle("active");
+//     // document.getElementById("sideNav").classList.toggle("active");
+
+// // Do something else, like open/close menu
+// });
+// function showEnv(){
+//     document.getElementById("env-extended").classList.toggle("env-active");
+// }
+
 
 
 
