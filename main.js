db35edacdf7a771a1b4732c5acb4bea594893299
Maple <charissamaple@gmail.com>
2020-03-11 00:57:41 -0700

    fixed hamburger menu javascript for homepage
    javascript toggle and css .active at breakpoint 420

--- main.js
+++ main.js
@@ -1,37 +1,9 @@
 function showMenu(){
+    var nav = document.getElementById("navigation");
 
-    var x = document.getElementById("navigation");
-    var y = screen.width;
-    var mobile = 420;
-
-    if (x.style.display === "none" && y < mobile) {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-
-    // if (y > mobile){
-    //     x.style.display = "block";
-    // }
+    nav.classList.toggle("active");
+    console.log(nav);
 }
-
-
-
-
-// var hamburger = document.querySelector(".burger");
-// // On click
-// hamburger.addEventListener("click", function() {
-// // Toggle class "is-active"
-// hamburger.classList.toggle("is-active");
-//     document.getElementById("sideNav").classList.toggle("active");
-//     document.getElementById("topNav").classList.toggle("active");
-//     // document.getElementById("sideNav").classList.toggle("active");
-
-// // Do something else, like open/close menu
-// });
-// function showEnv(){
-//     document.getElementById("env-extended").classList.toggle("env-active");
-// }
 
 
 
