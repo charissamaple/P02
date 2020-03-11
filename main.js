09f372db60bfd135d25987eda9e7fe062a46c4e3
Maple <charissamaple@gmail.com>
2020-03-11 01:25:53 -0700

    styled active navigation background, position, and display
    blue background, sticky position

--- main.js
+++ main.js
@@ -5,6 +5,36 @@ function showMenu(){
     console.log(nav);
 }
 
+// var burger_block;
+// var burger_html;
 
+// burger_block = document.createElement('div');
+// burger_html.innerHTML = 'haha';
 
+// burger_block = document.getElementById('navigation');
+// burger_block.appendChild(burger_html);
+
+// var block_to_insert ;
+// var container_block ;
+ 
+// block_to_insert = document.createElement( 'div' );
+// block_to_insert.innerHTML = 'This demo DIV block was inserted into the page using JavaScript.' ;
+ 
+// container_block = document.getElementById( 'democontainer' );
+// container_block.appendChild( block_to_insert );
+
+// <div id="democontainer">
+// <p>
+// The new block will appear below this paragraph.
+// </p>
+// </div>
+
+// <div id="democontainer">
+// <p>
+// The new block will appear below this paragraph.
+// </p>
+// <div>
+// This demo DIV block was inserted into the page using JavaScript.
+// </div>
+// </div>
 
